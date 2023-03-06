4bc8188c585e65ddb6c3997c39d04b8da1fc8db0
meggidy <mimamitchelle@gmail.com>
2023-03-06 11:04:46 +0300

    completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map(&:full_name)
+  end
+
+  def show_characters
+    Character.where(show_id: self.id)
+  end
 end
